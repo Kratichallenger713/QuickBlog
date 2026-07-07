6475d7a52cb1fadcb4b279313e23befd9490ff84
Kratichallenger713 <bkratika936@gmail.com>
2026-07-07 23:05:05 +0530

    fix: update CORS to allow Vercel frontend URL

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ const app = express();
 await connectDB()
 // Middleware 
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        process.env.FRONTEND_URL
+    ],
+    credentials: true
+}))
 app.use(express.json())
 
 //Routes 
